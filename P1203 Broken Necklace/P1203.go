83d9fef9dd9ef64853930bbf01cb5f55f72c8271
J-Catherine <865687826@qq.com>
2019-03-26 16:07:47 +0800

    some mistake occurs at P1203

--- P1203 Broken Necklace/P1203.go
+++ P1203 Broken Necklace/P1203.go
@@ -12,8 +12,8 @@ func main()  {
 	fmt.Scanln(&n)
 	fmt.Scanln(&s)
 	for i = 0; i < n; i++ {
-		if s[(i-1+n)%n] != 'w' && s[i] != 'w' {
-			for j = i; j < i + n; j++ {
+		if s[(i-1+n)%n] != 'w' && s[i] != 'w' { //找断开点
+			for j = i; j < i + n; j++ { //向右搜索直到不同
 				if s[j % n] == s[i] || s[j%n] == 'w'{
 					cntRight += 1
 				} else{
@@ -21,7 +21,11 @@ func main()  {
 					break
 				}
 			}
-			for j = i - 1; j >= i - n; j-- {
+			if cntRight == n {
+				maxCnt = cntRight
+				break
+			}
+			for j = i - 1; j >= i - n; j-- { //向左搜索直到不同
 				if s[(j + n) %n] == s[(i-1+n)%n] || s[(j+n)%n] == 'w' {
 					cntLeft += 1
 				} else {
