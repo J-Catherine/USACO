cdfef6a2edb2488c4d8a8161db553753a018a9f6
J-Catherine <865687826@qq.com>
2019-03-26 16:31:02 +0800

    P1203 with naive method O(n^2)

--- P1203 Broken Necklace/P1203.go
+++ P1203 Broken Necklace/P1203.go
@@ -4,20 +4,24 @@ import (
 	"fmt"
 )
 
-var n,i,j,cntRight,cntLeft,maxCnt int
-var s string
+var (
+	n, i, j, cntRight, cntLeft, maxCnt int
+	before                             uint8
+	s                                  string
+)
 
-
-func main()  {
+func main() {
 	fmt.Scanln(&n)
 	fmt.Scanln(&s)
 	for i = 0; i < n; i++ {
-		if s[(i-1+n)%n] != 'w' && s[i] != 'w' { //找断开点
-			for j = i; j < i + n; j++ { //向右搜索直到不同
-				if s[j % n] == s[i] || s[j%n] == 'w'{
+		cntLeft = 0
+		cntRight = 0
+		if s[i] != 'w' { //找断开点，下一个不为w才可断开
+			for j = i; j < i+n; j++ { //向右搜索直到不同
+				if s[j%n] == s[i] || s[j%n] == 'w' {
 					cntRight += 1
-				} else{
-					//fmt.Printf("right: %d %d  i %d",j%n,cntRight,i)
+				} else {
+					//fmt.Printf("right: %d %d  i %d\n", j%n, cntRight, i)
 					break
 				}
 			}
@@ -25,20 +29,32 @@ func main()  {
 				maxCnt = cntRight
 				break
 			}
-			for j = i - 1; j >= i - n; j-- { //向左搜索直到不同
-				if s[(j + n) %n] == s[(i-1+n)%n] || s[(j+n)%n] == 'w' {
-					cntLeft += 1
+			if s[(i-1+n)%n] != s[i] {
+				if s[(i-1+n)%n] == 'w' { // 若上一个为w
+					if s[i] == 'b' {
+						before = 'r'
+					} else {
+						before = 'b'
+					}
 				} else {
-					//fmt.Printf(" left: %d %d\n",(j + n) %n,cntLeft)
-					break
+					before = s[(i-1+n)%n]
+				}
+				for j = i-1; j >= i-n+cntRight; j-- { //向左搜索直到不同
+					if s[(j+n)%n] == before || s[(j+n)%n] == 'w' {
+						cntLeft += 1
+					} else {
+						//fmt.Printf(" left: %d %d\n", (j+n)%n, cntLeft)
+						break
+					}
+				}
+				if cntLeft+cntRight > maxCnt {
+					maxCnt = cntLeft + cntRight
 				}
 			}
-			if cntLeft + cntRight > maxCnt{
-				maxCnt = cntLeft + cntRight
-			}
-			cntLeft = 0
-			cntRight = 0
 		}
+	}
+	if maxCnt == 0 {
+		maxCnt = n
 	}
 	fmt.Println(maxCnt)
 }
