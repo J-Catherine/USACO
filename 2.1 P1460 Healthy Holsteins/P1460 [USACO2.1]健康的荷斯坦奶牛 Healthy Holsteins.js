67d384d94870846f4a294ae190062662f5240624
王佳羽 <wangjiayu.ada@bytedance.com>
2021-01-03 14:15:00 +0800

    feat: P1460/2.1: DFS

---
+++ 2.1 P1460 Healthy Holsteins/P1460 [USACO2.1]健康的荷斯坦奶牛 Healthy Holsteins.js
@@ -0,0 +1,65 @@
+const fs = require('fs')
+const data = fs.readFileSync('/dev/stdin', {encoding: 'ascii'})
+// const data = fs.readFileSync('./testdata.in', {encoding: 'ascii'})
+const result = data.trim().split('\n')
+const v = parseInt(result[0]);
+const vList = result[1].trim().split(' ').map(item => parseInt(item));
+const g = parseInt(result[2]);
+const gList = result.slice(3).map(item => item.split(' ').map(item => parseInt(item)));
+// console.log(v, vList, g,gList)
+
+const compareNormal = (v) => {
+    const normal = vList;
+    if(v.length !== normal.length) {
+        return false;
+    }
+    for(let i=0; i<normal.length; i++) {
+        if(v[i] < normal[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const addition = (a, b) => {
+    // console.log(a,b)
+    if(a.length !== b.length) { return a }
+    return a.map((item,idx) => item+b[idx])
+}
+
+let res = {
+    type: v,
+    typeList: []
+}
+const dfs = (idx, currSum, currType, typeList) => {
+    // console.log(idx, currSum, currType, typeList)
+    if(compareNormal(currSum)) {
+        if(currType <= res.type) {
+            // console.log("(*****)", currType, res.type, currSum, typeList)
+            res = {
+                type: currType,
+                typeList: typeList.slice()
+            }
+            return 
+        };
+    }
+    if(idx >= g) { return }
+    typeList[idx] = 1
+    // console.log(idx, gList[idx])
+    dfs(idx+1, addition(currSum, gList[idx]), currType+1, typeList);
+    typeList[idx] = 0
+    dfs(idx+1, currSum, currType, typeList);
+}
+
+const a = new Array(v);
+const b = new Array(v);
+for(let i=0;i<v;i++){
+    a[i] = 0
+    b[i] = 0
+}
+dfs(0, b, 0, a)
+
+let r = res.type.toString()
+res.typeList.map((item, idx)=>{if(item) {r+=` ${idx+1}`}})
+console.log(r);
+process.exit() // 请注意必须在出口点处加入此行
